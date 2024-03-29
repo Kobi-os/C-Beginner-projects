4f8a1ff41ccdea0d11c1df87002ca7a6d74cb129
Wojciech Kobier <125194358+Kobi-os@users.noreply.github.com>
2024-03-29 16:05:17 +0100

    Add files via upload

---
+++ CoinFlip.cs
@@ -0,0 +1,34 @@
+namespace CoinFlipGame
+{
+    public partial class CoinFlipGame : Form
+    {
+
+        int heads = 0;
+        int tails = 0;
+        public CoinFlipGame()
+        {
+            InitializeComponent();
+        }
+
+        private void CoinFlipButton_Click(object sender, EventArgs e)
+        {
+            Random coinFlip = new Random();
+            int result = coinFlip.Next(1, 3);
+
+            if (result == 1)
+            {
+                CoinImage.ImageLocation = @"C:\Users\Wojtek\Desktop\ZDJ, PDF ITP\branding.png";
+                CoinImage.SizeMode = PictureBoxSizeMode.StretchImage;
+                heads++;
+                HeadsLabel.Text = $"Heads: {heads}";
+            }
+            else
+            {
+                CoinImage.ImageLocation = @"C:\Users\Wojtek\Desktop\ZDJ, PDF ITP\ai.png";
+                CoinImage.SizeMode = PictureBoxSizeMode.StretchImage;
+                tails++;
+                TailsLabel.Text = $"Tails: {tails}";
+            }
+        }
+    }
+}
